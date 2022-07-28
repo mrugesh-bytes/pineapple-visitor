5e85a95c6a6f2a8a33dd4322f6f9d2fddf39a156
Vijay <vijay.bapodara@bytestechnolab.in>
2022-07-28 16:12:13 +0530

    Unit Card Design

--- src/components/reserve/confirm/dashboard/Dashboard.tsx
+++ src/components/reserve/confirm/dashboard/Dashboard.tsx
@@ -6,30 +6,34 @@ import { useLocation } from 'react-router-dom';
 import moment from 'moment';
 
 const Dashboard = () => {
-    const location: any = useLocation();
-    const selectedDate = location.state.selectedDate;
-    return (
-        <div className={styles.dashboardWrapper}>
-            <h2 className={styles.title}>Schedule a visit for B</h2>
-            <div className={styles.dashboardBanner}>
-                <img src={bannerImg} alt="banner" />
-            </div>
-            <div className={styles.scheduleInfo}>
-                <p>All done! You will recieve an SMS message with more information once confirmed.</p>
-                <div className={styles.schedule}>
-                    <div className={styles.date}>
-                        <div className={styles.scheduleIcon}>
-                            <img src={iconCalender} alt="Icon Date" />{' '}
-                        </div>
-                        <span>{moment(selectedDate).format('DD-MM-YYYY')}</span>
-                    </div>
-                </div>
-                <div className={styles.btnWrapper}>
-                    <button>Confirm</button>
-                </div>
-            </div>
-        </div>
-    );
+	const location: any = useLocation();
+	const selectedDate = location.state.selectedDate;
+	return (
+		<div className={styles.dashboardWrapper}>
+			<h2 className={styles.title}>Schedule a visit for B</h2>
+			<div className={styles.dashboardBanner}>
+				<img src={bannerImg} alt="banner" />
+			</div>
+			<div className={styles.scheduleInfo}>
+				{/* <h2 className={styles.confirmTitle}>Appointment Confirmed</h2> */}
+				<p>
+					All done! You will recieve an SMS message with more
+					information once confirmed.
+				</p>
+				<div className={styles.schedule}>
+					<div className={styles.date}>
+						<div className={styles.scheduleIcon}>
+							<img src={iconCalender} alt="Icon Date" />{" "}
+						</div>
+						<span>{moment(selectedDate).format("DD-MM-YYYY")}</span>
+					</div>
+				</div>
+				<div className={styles.btnWrapper}>
+					<button>Confirm</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
